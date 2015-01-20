14378a8ff8ca2f2828d557cef718e7985246ecdd
AshBardhan <ashbardhan@gmail.com>
2015-01-21 00:34:46 +0530

    Setup LESS Architecture

--- public/js/user.js
+++ public/js/user.js
@@ -21,15 +21,21 @@ var userFlow = {
     setInterval(function () {
       userFlow.fetchNotification(false);
     }, 10000);
-    $('.notify-icon').on('click', function () {
+    $('.notify-icon').on('click', function (e) {
+      e.stopPropagation();
       $('.notify-box').fadeIn();
       localStorage.setItem('count', 0);
       $('#yes-list .message').removeClass('new');
     });
     $(document).on('keyup', function (e) {
-      if (e.which == 27 && $('.notify-box').is(':visible')) {
+      if (e.which == 27 && $('.error-box, .error-background').is(':visible')) {
+        $('.error-box, .error-background').fadeOut();
+      }
+    });
+    $(document).on('click', function (e) {
+      if ($('.notify-box').is(':visible')) {
         $('.notify-box').fadeOut();
-        $('.notify-count, .notify-total-count').addClass('hide');
+        $('.notify-count, .notify-total-count').addClass('hide').removeClass('display-inline');
       }
     });
   },
@@ -47,11 +53,11 @@ var userFlow = {
           }
           $('#yes-list').removeClass('hide').prepend(message);
           $('#no-list').addClass('hide');
-          $('.notify-count, .notify-total-count').text(localStorage.count <= 99 ? localStorage.count : '99+').removeClass('hide');
+          $('.notify-count, .notify-total-count').text(localStorage.count <= 99 ? localStorage.count : '99+').removeClass('hide').addClass('display-inline');
         }
       }
       var failure = function (data) {
-        console.log(data);
+        $('.error-box, .error-background').fadeIn();
       }
       userFlow._sendAJAXRequest(urls.fetchNotification, {count: count}, "GET", success, failure, "JSON", "application/x-www-form-urlencoded; charset=UTF-8");
     }
